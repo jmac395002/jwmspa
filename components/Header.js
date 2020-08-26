4c0559014b48c6a58452ff607b4eefe7dfb44f35
Jeffery McMillian <jmac395002@gmail.com>
2020-08-26 01:59:08 -0500

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,12 @@
 export default()  => `
 <header>
   <h1 class="logo"></h1>
+  <nav>
+<ul>
+  <li><a href="index.html">Home</a></li>
+  <li><a href="index2.html">Project Reviews</a></li>
+  <li><a href="index3.html">Future Projects</a></li>
+</ul>
+</nav>
 </header>
 `;
--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,3 @@
 export default()  => `
-<nav>
-<ul>
-  <li><a href="index.html">Home</a></li>
-  <li><a href="index2.html">Project Reviews</a></li>
-  <li><a href="index3.html">Future Projects</a></li>
-</ul>
-</nav>
+
 `;
